7c4a8d61bf5fb1241e287d0e2e10b3290ee293b6
Ihor Omelchenko <iom@ggr.group>
2024-12-14 03:28:12 +0200

    init project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {RouterOutlet} from '@angular/router';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  imports: [RouterOutlet]
 })
 export class AppComponent {
-  title = 'shop.so';
+
 }
